851c0445d597bf20cf9a8608b1e8301e3ee13ba9
Luis Miguel <luibelzan@outlook.com>
2024-03-11 13:56:31 +0100

    Database validation on update equipment form

--- app/equipment/update/[slug]/page.js
+++ app/equipment/update/[slug]/page.js
@@ -8,13 +8,16 @@ import * as Yup from 'yup';
 export default function EquipmentUpdate() {
 
     const [equip, setEquip] = useState();
+    const [cts, setCts] = useState([]);
+    const [dists, setDists] = useState([]);
+    const [trafos, setTrafos] = useState([]);
 
     const path = usePathname();
     const query = path.split('/').pop();
     const objetoJSON = { idEquip: query };
 
     useEffect(() => {
-        async function fetchEquip() {
+        async function fetchData() {
             try {
                 const res = await fetch('/api/equipment/findEquipById', {
                     method: 'POST',
@@ -25,11 +28,17 @@ export default function EquipmentUpdate() {
                 });
                 const data = await res.json();
                 setEquip(data);
+                const res2 = await fetch('/api/ct/getAllCts');
+                const res3 = await fetch('/api/distributors/getAllDist');
+                const res4 = await fetch('/api/trafos/getAllTrafos');
+                setCts(await res2.json());
+                setDists(await res3.json());
+                setTrafos(await res4.json());
             } catch(error) {
                 console.error('Error: ', error);
             }
         }
-        fetchEquip();
+        fetchData();
     }, []);
 
     const [isSubmitted, setIsSubmitted] = useState(false);
@@ -47,9 +56,15 @@ export default function EquipmentUpdate() {
         validationSchema: Yup.object({
           tipEquip: Yup.string().required('La descripción es obligatoria').matches(/^(SABT|VTN|DC)/, 'This field must match SABT, VTN or DC'),
           nomEquip: Yup.string().required('La descripción es obligatoria'),
-          idTrafo: Yup.string().required('La descripción es obligatoria').matches(/^Trf-/, 'This field must match Trf-'), 
-          idCt: Yup.string().required('La descripción es obligatoria').matches(/^CT-/, 'This field must match Trf-'), 
-          idDist: Yup.string().required('La descripción es obligatoria'),
+          idTrafo: Yup.string().required('La descripción es obligatoria').matches(/^Trf-/, 'This field must match Trf-').test('idTrafo-valid', 'This Id_Trafo doesnt exist', async(value) => {
+            return trafos.some(trafo => trafo.id_trafo === value)
+          }),  
+          idCt: Yup.string().required('La descripción es obligatoria').matches(/^CT-/, 'This field must match Trf-').test('idCt-valid', 'This Id_Ct doesnt exist', async(value) => {
+            return cts.some(ct => ct.id_ct === value)
+          }), 
+          idDist: Yup.string().required('La descripción es obligatoria').test('idDist-valid', 'This Id_Dist doesnt exist', async(value) => {
+            return dists.some(dist => dist.id_distribuidora === value)
+          }),
         }),
         onSubmit: async (values, { setSubmitting }) => {
           const concatenatedJSON = { ...objetoJSON, ...values };
